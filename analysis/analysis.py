6ab43342081727da0a319dff73a3c55c7d9e5127
Alex Aksentyev <alexaksentyev@Alexs-MacBook-Pro.local>
2019-11-21 16:45:03 +0300

    working (gives somewhat expected pictures) version

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,14 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt; plt.ion()
 
-d_type = list(zip(['TURN','PID'], [int]*2)) + list(zip(['X','A','Y','B','T','D'], [float]*6))
-dat = np.loadtxt('data/TEST/TRPRAY:RUN.dat', d_type, skiprows=2)
-nray = dat['PID'].max() + 1
+HOME = '/Users/alexaksentyev/REPOS/NICA-FS/'
+
+d_type = list(zip(['iteration','EID', 'ray'], [int]*3)) + list(zip(['X','A','Y','B','T','D'], [float]*6))
+dat = np.loadtxt(HOME+'data/TEST/TRPRAY:RUN.dat', d_type, skiprows=2)
+nray = dat['ray'].max() + 1
 dat.shape = (-1, nray)
 
 
 fig, ax = plt.subplots(2,1, sharex=True)
-ax[0].plot(dat['X'][:,1])
-ax[0].set_ylabel('X')
-ax[1].plot(dat['Y'][:, 1])
-ax[1].set_ylabel('Y')
+df = dat[:, 19:]
+ax[0].plot(#df['EID'][:19],
+               df['X'][:19])
+ax[0].set_xlabel('EID'); ax[0].set_ylabel('X')
+ax[1].plot(#df['EID'][:19],
+               df['Y'][:19])
+ax[1].set_xlabel('EID'); ax[1].set_ylabel('Y')
