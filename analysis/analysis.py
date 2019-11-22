e4bc2ceedd67c2cd44e66f914d3341f1fa8dcb81
Alex Aksentyev <alexaksentyev@Alexs-MBP.Dlink>
2019-11-22 19:44:05 +0300

    minor update to test.fox and analysis.py

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,26 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt; plt.ion()
 
+case = ('trel', 'ps')
+
+filename = {'ps':'TRPRAY', 'sp':'TRPSPI'}
 HOME = '/Users/alexaksentyev/REPOS/NICA-FS/'
-VARS = list(zip(['X','A','Y','B','T','D'], [float]*6))
-d_type_trel = list(zip(['iteration','EID', 'ray'], [int]*3)) + VARS
-d_type_tr = list(zip(['turn', 'PID'], [int]*2)) + VARS
-dat = np.loadtxt(HOME+'data/TEST/TRPRAY:RUN.dat', d_type_tr, skiprows=2)
+VARS = {
+    'ps': list(zip(['X','A','Y','B','T','D'], [float]*6)),
+    'sp': list(zip(['S_X','S_Y','S_Z'], [float]*3))
+    }
+d_type = {
+    'trel': list(zip(['iteration','EID', 'PID'], [int]*3)) + VARS[case[1]],
+    'tr': list(zip(['turn', 'PID'], [int]*2)) + VARS[case[1]]
+    }
+dat = np.loadtxt(HOME+'data/TEST/{}:{}.dat'.format(filename[case[1]], case[0].upper()), d_type[case[0]], skiprows=2)
 nray = dat['PID'].max() + 1
 dat.shape = (-1, nray)
 
 
 n=30
+pref = {'ps':'', 'sp':'S_'}
 fig, ax = plt.subplots(2,1, sharex=True)
 df = dat[:, 17:]
 ax[0].plot(#df['EID'][:19],
-               df['X'][:n])
+               df[pref[case[1]]+'X'][:n])
 #ax[0].set_xlabel('EID');
-ax[0].set_ylabel('X')
+ax[0].set_ylabel(pref[case[1]]+'X')
 ax[1].plot(#df['EID'][:19],
-               df['Y'][:n])
+               df[pref[case[1]]+'Y'][:n])
 #ax[1].set_xlabel('EID');
-ax[1].set_ylabel('Y')
-fig, ax = plt.subplots(2,1)
-ax[0].plot(df['X'][:n], df['A'][:n], '.')
-ax[1].plot(df['Y'][:n], df['B'][:n], '.')
+ax[1].set_ylabel(pref[case[1]]+'Y')
+# fig, ax = plt.subplots(2,1)
+# ax[0].plot(df['X'][:n], df['A'][:n], '.')
+# ax[1].plot(df['Y'][:n], df['B'][:n], '.')
