28b05977915717c1fdbd6ad565746c2eefc03c53
Alex Aksentyev <alexaksentyev@Alexs-MacBook-Pro.local>
2019-12-03 16:45:53 +0300

    converter-for-trel.py converts .madx file into an array of maps

--- analysis/converter-for-trel.py
+++ analysis/converter-for-trel.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 HOME = '/Users/alexaksentyev/REPOS/NICA-FS/'
 
 el_dict = {
@@ -8,7 +10,7 @@ el_dict = {
     'SOLENOID': ('SOLENOID',2)
     }
 
-
+el_lbls = []
 fout = open(HOME+'src/setups/nica_24sol_rbend-sequence.fox','w')
 with open(HOME+'madx-scripts/nica_24sol_rbend.seq', 'r') as fin:
     for cnt, line in enumerate(fin):
@@ -17,10 +19,11 @@ with open(HOME+'madx-scripts/nica_24sol_rbend.seq', 'r') as fin:
             com = com.strip(':')
             elem = elem.strip(';\n').split(',')
             elem_name, n_arg = el_dict[elem[0]]
+            el_lbls.append(elem_name)
             n_zeros = n_arg-len(elem[1:])
             elem.extend(['name=0' for i in range(n_zeros)])
             out_string = 'UM; '
-            out_string += el_dict[elem[0]][0] + ' '
+            out_string += elem_name + ' '
             for par in elem[1:]:
                 par_val = par.split('=')[1]
                 if (par_val.find('+',1)>0 or par_val.find('-',1)>0):
@@ -35,3 +38,6 @@ with open(HOME+'madx-scripts/nica_24sol_rbend.seq', 'r') as fin:
             fout.close()
         
 fout.close()
+
+el_lbls = np.array(el_lbls)
+np.save(HOME+'analysis/nica_element_names', el_lbls)
