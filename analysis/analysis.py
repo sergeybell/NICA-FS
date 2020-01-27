d64dfcf89d468448d807012c00248c255e36e17c
Alex Aksentyev <alexaksentyev@Alexs-MBP.Dlink>
2020-01-27 13:52:48 +0300

    analysis.py cleaned up

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,22 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt; plt.ion()
 
-## for 3d animation
-from mpl_toolkits.mplot3d import Axes3D, proj3d
-from matplotlib import animation
-
-def get_arrow(index, S_X, S_Y, S_Z):
-    return 0, 0, 0, S_X[index], S_Y[index], S_Z[index]
-
-def update(index, S_X, S_Y, S_Z):
-    global quiver
-    quiver.remove()
-    quiver = ax.quiver(*get_arrow(0, S_X, S_Y, S_Z), color='r')
-    quiver = ax.quiver(*get_arrow(-1, S_X, S_Y, S_Z), color='k')
-    quiver = ax.quiver(*get_arrow(index, S_X, S_Y, S_Z))
-
-##
-
 class Analysis:
     def __init__(self, data):
         self._data = data
@@ -25,3 +9,15 @@ class Analysis:
         S = {lbl:data['S_'+lbl] for lbl in ['X','Y','Z']}
         nray = S['X'].shape[1]
         P = {lbl: np.sum(S[lbl],axis=1)/nray for lbl in ['X','Y','Z']}
+        self._Pvec = P
+        self._Pval = np.sqrt(self._Pvec['X']**2 + self._Pvec['Y']**2 + self._Pvec['Z']**2)
+
+    @property
+    def Pvec(self):
+        return self._Pvec
+    @property
+    def Pval(self):
+        return self._Pval
+    @property
+    def S(self):
+        return self._spin
