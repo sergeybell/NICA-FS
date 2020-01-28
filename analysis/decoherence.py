992e4a9b69f678d9e5f915df15de7feb8baf58c1
Alex Aksentyev <alexaksentyev@Alexs-MacBook-Pro.local>
2020-01-28 16:53:13 +0300

    minor changes to decoherence .fox and .py

--- analysis/decoherence.py
+++ analysis/decoherence.py
@@ -28,33 +28,35 @@ def plot_decoh_meas(data):
         ax[i].plot(data['turn'], dm[lbl])
         ax[i].set_ylabel('RMS(S_{})'.format(lbl))
 
+def load_data(path):
+    VARS = {
+        'ps': list(zip(['X','A','Y','B','T','D'], [float]*6)),
+        'sp': list(zip(['S_X','S_Y','S_Z'], [float]*3))
+        }
+    d_type = lambda case: list(zip(['turn', 'PID'], [int]*2)) + VARS[case]
+    
+    dat = {
+        'sp' : np.loadtxt(path + 'TRPSPI:TR.dat', d_type('sp'), skiprows=2),
+        'ps':  np.loadtxt(path + 'TRPRAY:TR.dat', d_type('ps'), skiprows=2)
+        }
+    nray = dat['sp']['PID'].max() + 1
+    for el in ['sp','ps']:
+        dat[el].shape = (-1, nray)
+        dat[el] = dat[el][:,1:]
+    nray -= 1
+    return dat, nray
+##
 
 HOME = '/Users/alexaksentyev/REPOS/NICA-FS/'
-NICA_EL_LBLS = np.insert(np.load('nica_element_names.npy'),0,'INJ')
-NICA_EL_LBLS = np.array([e+' ['+ str(i) + ']' for i,e in enumerate(NICA_EL_LBLS)])
-VARS = {
-    'ps': list(zip(['X','A','Y','B','T','D'], [float]*6)),
-    'sp': list(zip(['S_X','S_Y','S_Z'], [float]*3))
-    }
-d_type = lambda case: list(zip(['turn', 'PID'], [int]*2)) + VARS[case]
-    
-dat = {
-    'sp' : np.loadtxt(HOME+'data/DECOH/SHORT/TRPSPI:TR.dat', d_type('sp'), skiprows=2)#,
-    # 'ps': np.loadtxt(HOME+'data/DECOH/TRPRAY:TR.dat', d_type('ps'), skiprows=2)
-    }
-nray = dat['sp']['PID'].max() + 1
-for el in ['sp']:#,'sp']:
-    dat[el].shape = (-1, nray)
-    dat[el] = dat[el][:,1:]
+DIR = 'data/DECOH/SHORT/INJECT_SPIN/OPTIMAL/'
+dat, nray = load_data(HOME + DIR)
 
+bunch_num = 4
+a = ana.Analysis(dat['sp'][:, np.arange(bunch_num-1, nray, 4)])
+# S_X, S_Y, S_Z = (a.S[lbl][np.arange(0, 1001, 50)] for lbl in ['X','Y','Z'])
 
-a = ana.Analysis(dat['sp'])
-# where = np.arange(0, 1001, 100)
-# rays = np.arange(0, 20, 1)
-# S_X, S_Y, S_Z = (a.S[lbl][where][:, rays] for lbl in ['X','Y','Z'])
-
-# fig = plt.figure()
-# ax = fig.add_subplot(111, projection='3d')
+# fig0 = plt.figure()
+# ax = fig0.add_subplot(111, projection='3d')
 # ax.set_xlabel('X')
 # ax.set_ylabel('Z')
 # ax.set_zlabel('Y')
@@ -64,7 +66,7 @@ a = ana.Analysis(dat['sp'])
 
 # quiver = ax.quiver(*get_arrow(0, S_X, S_Z, S_Y))
 
-# anim = animation.FuncAnimation(fig, update, fargs=(S_X, S_Z, S_Y), interval=10, blit=False, frames=S_X.shape[0])
+# anim = animation.FuncAnimation(fig0, update, fargs=(S_X, S_Z, S_Y), interval=10, blit=False, frames=S_X.shape[0])
 # anim.save('../reports/decoherence_10e6.gif')
 
 fig, ax = plt.subplots(1,1)
@@ -79,4 +81,26 @@ popt = yy[1], (yy[-1]-yy[1])/(nn[-1]-nn[1])/ftr
 # popt, pcov = curve_fit(lambda x, a,b: a + b*x, nn, yy)
 ax.plot(nn*ftr, popt[0] + popt[1]*nn*ftr, 'r-', label=r'$(\beta_0 = {:4.2e}, \beta_1 = {:4.2e})$'.format(*popt))
 ax.legend()
+ax.set_title(DIR)
 
+# def init():  # only required for blitting to give a clean slate.
+#     line.set_ydata([np.nan] * X.shape[1])
+#     return line,
+
+
+# def animate(i):
+#     line.set_data(X[i], A[i]) # update the data.
+#     return line,
+
+#elems = np.concatenate((np.arange(0,50,10), np.arange(100,400, 10), np.array([420, 450, 470, 471])))
+# X, A = (dat['ps'][lbl] for lbl in ['T','D'])
+
+# fig1, ax = plt.subplots()
+# ax.set_xlim(-2e-3,2e-3)
+# ax.set_ylim(-2e-3, 2e-3)
+
+# line, = ax.plot(X[0], A[0], 'r.')
+
+
+# ani = animation.FuncAnimation(fig1, animate, #init_func=init,
+#                                   interval=200, blit=False, save_count=50, frames=X.shape[0])
