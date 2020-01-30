e8abdc1ac9a28b7a9a99fe3281cb510a289fd745
Alex Aksentyev <alexaksentyev@Alexs-MacBook-Pro.local>
2020-01-30 14:11:54 +0300

    analysis: test.py for data from test.fox

---
+++ analysis/test.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt; plt.ion()
+
+HOME = '/Users/alexaksentyev/REPOS/NICA-FS/'
+DIR  = 'data/TEST/'
+
+VARS_PS = list(zip(['X','A','Y','B','T','D'], [float]*6))
+D_TYPE_PS = list(zip(['iteration', 'PID'], [int]*2)) + VARS_PS
+ps =  np.loadtxt(HOME+DIR+'TRPRAY:TR.dat', D_TYPE_PS, skiprows=2)
+nray = ps['PID'].max() + 1
+ps.shape = (-1, nray)
+ps = ps[:,1:]; nray -= 1 # removing the zero ray
+
+#ii = np.arange(3, nray, 4)
+#bunch = ps[:, ii]
